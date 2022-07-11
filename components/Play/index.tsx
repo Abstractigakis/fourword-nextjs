25119eac5db2a59b3e44ffaad4ecc3f80c44683b
abstractigakis <abstractigakis@gmail.com>
2022-07-11 17:25:12 -0400

    fixed ts error

--- components/Play/index.tsx
+++ components/Play/index.tsx
@@ -1,4 +1,11 @@
-const Play = () => {
+import { IFaunaUser } from "@lib/faunadb/types";
+import { FC } from "react";
+
+export interface IPlayProps {
+  faunaUser: IFaunaUser;
+}
+
+const Play: FC<IPlayProps> = ({ faunaUser }) => {
   return <div>Play</div>;
 };
 
