925a75b07468912f2b910ae2bf762bce3f4032e5
abstractigakis <abstractigakis@gmail.com>
2022-07-11 17:23:21 -0400

    play page

---
+++ pages/play.tsx
@@ -0,0 +1,32 @@
+import Play from "@components/Play";
+import { useFaunaUserQuery } from "hooks";
+import { withCustomServerSidePageAuth } from "@lib/nextAuth/middleware/server/pages";
+import { GetServerSideProps, NextPage } from "next";
+import PageLoading from "@components/PageLoading";
+
+interface IPlayPageProps {
+  data: { authUserEmail: string; puzzleId: string };
+}
+
+const PlayPage: NextPage<IPlayPageProps> = ({ data }) => {
+  const { authUserEmail } = data;
+
+  const faunaUserQuery = useFaunaUserQuery(authUserEmail);
+  const faunaUser = faunaUserQuery.data;
+
+  return (
+    <>
+      {faunaUserQuery.status === "loading" && <PageLoading />}
+      {faunaUserQuery.status === "success" && <Play faunaUser={faunaUser} />}
+      {faunaUserQuery.status === "error" && (
+        <pre>{JSON.stringify(faunaUserQuery.error, null, 2)}</pre>
+      )}
+    </>
+  );
+};
+
+export default PlayPage;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  return await withCustomServerSidePageAuth(context);
+};
